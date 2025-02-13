7b78dad450c20e95664bbc1e85255849f5307fbe
RyyNotavailable <48169572+ryankputra@users.noreply.github.com>
2025-02-13 14:06:58 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -151,7 +151,7 @@ async function sendMainMenu(ctx) {
   }
 
   const messageText = `*Selamat datang di ${NAMA_STORE},
-Powered by NEWBIE* 🚀
+Powered by RYYSTORE 🚀
 Bot VPN serba otomatis untuk membeli
 layanan VPN dengan mudah dan cepat
 Nikmati kemudahan dan kecepatan
